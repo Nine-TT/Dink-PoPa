b6f53276e2695c8077c022fddfad647dccdf1e94
Chin <chin.tt.dev@gmail.com>
2022-12-03 20:27:09 +0700

    commit first

---
+++ js/app.js
@@ -0,0 +1,49 @@
+
+
+
+$(document).ready(function () {
+    $("#main").load("../layout/home/main.html");
+})
+
+$(document).ready(function () {
+    $("#footer").load("../layout/footer/footer.html");
+})
+
+function clickTab(path) {
+    $("#product-love-tab").empty();
+    $("#product-love-tab").load(path);
+
+}
+
+$(document).ready(function () {
+    $(".menu-love-item").click(function () {
+        $(".menu-love-item").removeClass("active");
+        $(".menu-love-item").addClass("active");
+    });
+});
+
+
+
+
+
+
+function clickTabMenuHeader(path) {
+    $("#main").empty();
+    $("#main").load(path)
+}
+
+$(document).ready(function () {
+    
+
+    $(".active-class ").click(function () {
+        $(".active-class ").removeClass("active");
+        $(this).addClass("active");
+    });
+});
+
+
+
+
+
+
+
---
+++ layout/home/main.js
@@ -0,0 +1,65 @@
+
+window.onload = function () {
+    /* lấy phần tử modal */
+    var modal = document.getElementById("myModal");
+    var product_name = document.getElementById("productName").textContent;
+
+    /* thiết lập nút mở modal */
+    // var btn = document.getElementById("btn-buy");
+    var btn = document.querySelectorAll(".buy-btn");
+
+    var imgArr = []
+
+    btn.forEach(element => {
+        var product_name = element.parentElement.parentElement.querySelector(".product-name").textContent;
+        var product_img = element.parentElement.parentElement.querySelector("img").src;
+
+
+
+        element.onclick = function () {
+            modal.style.display = "block";
+            amount++;
+            document.getElementById("modal-product-name").innerHTML = product_name;
+            document.getElementById("modal-product-amount").innerHTML = amount;
+            document.getElementById("header-product-amount").innerHTML = amount;
+
+            imgArr = [...imgArr, product_img]
+            var tagImg = ``
+            imgArr.forEach((value) => {
+                tagImg += `<img src="${value}" alt="">`
+
+            })
+
+            document.getElementById("product-image").innerHTML = tagImg
+
+
+
+            console.log(imgArr)
+        }
+    });
+
+    //myBtn
+
+    /* thiết lập nút đóng modal */
+    var span = document.getElementsByClassName("close")[0];
+
+    var amount = 0
+
+    document.getElementById("header-product-amount").innerHTML = amount;
+
+    /* Sẽ hiển thị modal khi người dùng click vào */
+
+
+    /* Sẽ đóng modal khi nhấn dấu x */
+    span.onclick = function () {
+        modal.style.display = "none";
+    }
+
+    /*Sẽ đóng modal khi nhấp ra ngoài màn hình*/
+    window.onclick = function (event) {
+        if (event.target == modal) {
+            modal.style.display = "none";
+        }
+    }
+
+};
